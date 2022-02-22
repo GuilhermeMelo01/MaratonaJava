2d04eee98d58be05afeececa9da2206bfe734f66
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-02-22 16:13:05 -0300

    Aula JDBC do Java DevDojo

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -5,21 +5,66 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
-    public static void save(Producer producer){
+    public static void save(Producer producer) {
         String sql;
         sql = "INSERT INTO producer (name) values ('%s')".formatted(producer.getName());
-        try(Connection conn = ConnectionFactory.getConnection()){
+        try (Connection conn = ConnectionFactory.getConnection()) {
             Statement stmt = conn.createStatement();
             int rowsAffected = stmt.executeUpdate(sql);
-            log.info("Insert producer in the database, rows affected '{}'", rowsAffected);
+            log.info("Insert producer '{}' in the database, rows affected '{}'", producer.getName(), rowsAffected);
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
         }
+    }
 
+    public static void delete(int id) {
+        String sql = "DELETE FROM producer WHERE (id = '%d')".formatted(id);
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            Statement stmt = conn.createStatement();
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Delete producer '{}' in the database, rows affected '{}'", id, rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    public static void update(Producer producer) {
+        String sql = "UPDATE `producer` SET `name` = '%s' WHERE (`id` = '%d')".formatted(producer.getName(), producer.getId());
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            Statement stmt = conn.createStatement();
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("update producer '{}' in the database, rows affected '{}'", producer.getId(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+        }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("find all producers");
+        String sql = "SELECT id, name FROM producer;";
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producer ", e);
+        }
+        return producers;
     }
 }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -3,18 +3,20 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 @Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
-
-//        log.info("info");
-//        log.debug("debug");
-//        log.warn("warn");
-//        log.error("error");
-//        log.trace("trace");
+        Producer producer = Producer.builder().name("MadHouse").build();
+        Producer producerToUpdate = Producer.builder().id(6).name("LEFTSIDE").build();
+        //  ProducerRepository.save(producer);
+        //  ProducerService.delete(4);
+//            ProducerService.update(producerToUpdate);
+        List<Producer> producers = ProducerService.findAll();
+        log.info("Producers found '{}'", producers);
     }
 }
