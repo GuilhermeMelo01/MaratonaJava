3af146e67a3dca61a7be596d7c89a6c2efdf0524
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-03-03 13:35:33 -0300

    aula 274 JDBC do Java DevDojo

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepositoryRowSet.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepositoryRowSet.java
@@ -2,8 +2,11 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.listener.CustomRowSetListener;
 
+import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.JdbcRowSet;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +31,37 @@ public class ProducerRepositoryRowSet {
             e.printStackTrace();
         }
         return producers;
+    }
+
+    public static void updateJdbcRowSet(Producer producer){
+        String sql = "SELECT * FROM producer WHERE (id = ?);";
+        try(JdbcRowSet jrs = ConnectionFactory.getjdbcRowSet()) {
+            jrs.addRowSetListener(new CustomRowSetListener());
+            jrs.setCommand(sql);
+            jrs.setInt(1, producer.getId());
+            jrs.execute();
+            if (!jrs.next()) return;
+            jrs.updateString("name", producer.getName());
+            jrs.updateRow();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateCachedRowSet(Producer producer){
+        String sql = "select * from producer WHERE (id = ?);";
+        try(CachedRowSet crs = ConnectionFactory.getCachedRowSet();
+        Connection connection = ConnectionFactory.getConnection()) {
+            connection.setAutoCommit(false);
+            crs.setCommand(sql);
+            crs.setInt(1, producer.getId());
+            crs.execute(connection);
+            if (!crs.next()) return;
+            crs.updateString("name", producer.getName());
+            crs.updateRow();
+            crs.acceptChanges();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
@@ -4,12 +4,14 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerServiceRowSet;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.List;
 
 @Log4j2
 public class ConnectionFactoryTest02 {
     public static void main(String[] args) {
-        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("MAD");
-        log.info(producers);
+        Producer producerToUpdate = Producer.builder().id(1).name("GOODHOUSE").build();
+        ProducerServiceRowSet.updateCachedRowSet(producerToUpdate);
+//        log.info("------------------");
+//        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("left");
+//        log.info(producers);
     }
 }
